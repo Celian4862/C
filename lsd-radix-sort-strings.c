9e575b3c5cead826295af94dc9fa5509aa528819
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2025-01-22 22:10:48 +0800

    Modified lsd-radix-sort-strings.c

--- lsd-radix-sort-strings.c
+++ lsd-radix-sort-strings.c
@@ -1,6 +1,25 @@
+/**
+ * Return codes:
+ * 0: Success
+ * 1: Not enough arguments
+ * 2: Too many arguments
+ * 3: Error opening file
+ * 4: Memory allocation failed
+ * 5: File format error
+ *
+ * Note: when using this programme, please ensure
+ * the following:
+ * - The file contains words separated by newlines.
+ * - The words contain only letters and numbers.
+ * - The file does not contain any spaces.
+ * - The last word in the file has only one empty
+ *   line after it; no more, no less.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 int main(int argc, char **argv)
 {
@@ -15,14 +34,16 @@ int main(int argc, char **argv)
     return 2;
   }
 
-  long file_size,           // Variable to store the file size
-      i;                    // Loop variable
-  int character_count = -1, // Variable to store the number of characters in a word
-      word_count = 0,       // Store the number of words to allocate space
-      word_i = 0;           // Iterator for the array of words
-  FILE *fp;
+  long file_size,          // Variable to store the file size
+      i;                   // Loop variable
+  int character_count = 0, // Variable to store the number of characters in a word
+      word_count = 0,      // Store the number of words to allocate space
+      word_i = 0;          // Iterator for the array of words
+  FILE *fp;                // File pointer
+  char **strs,             // Array of words
+      *buffer;             // Buffer to store the file contents
 
-  fopen(argv[1], "rb"); // Any subsequent file operation except fclose leads to segmentation fault... Why?
+  fp = fopen(argv[1], "rb");
   if (fp == NULL)
   {
     printf("Error opening file.\n");
@@ -35,11 +56,7 @@ int main(int argc, char **argv)
   file_size = ftell(fp);
   fseek(fp, 0L, SEEK_SET);
 
-  char *buffer = (char *)malloc(sizeof(char) * file_size); // Buffer to store the file contents
-
-  /**
-   * Currently require a buffer to store each word from the file
-   */
+  buffer = (char *)malloc(sizeof(char) * file_size);
 
   if (NULL == buffer)
   {
@@ -50,42 +67,79 @@ int main(int argc, char **argv)
   fread(buffer, 1, file_size, fp);
   fclose(fp);
 
+  // Count the number of words in the file.
+  // This does not help optimise performance, but it optimises storage.
   for (i = 0; i < file_size; i++)
   {
     if (buffer[i] == '\n')
+    {
+      continue;
+    }
+    if (buffer[i] == '\r') // Skip carriage return characters
     {
       word_count++;
     }
     else if (buffer[i] == ' ')
     {
       printf("Please format the file to contain words only separated by newlines.\n");
+      free(buffer);
+      return 5;
+    }
+    else if (!isalnum(buffer[i]))
+    {
+      printf("Please use only numbers and letters in the words.\n");
+      free(buffer);
+      return 5;
     }
   }
 
-  char **strs = (char **)malloc(sizeof(char *) * word_count);
+  strs = (char **)malloc(sizeof(char *) * word_count);
 
-  for (i = 0; i < file_size; i++)
+  if (strs == NULL)
   {
-    if (buffer[i] == '\n')
+    printf("Memory allocation failed.\n");
+    return 4;
+  }
+
+  for (i = 0; i < file_size; i++) // Store the words in the array of strings.
+  {
+    if (buffer[i] == '\r')
     {
-      strs[word_i++] = (char *)malloc(sizeof(char) * character_count + 1);
+      strs[word_i] = (char *)malloc(sizeof(char) * (character_count + 1));
+      if (strs[word_i] == NULL)
+      {
+        printf("Memory allocation failed.\n");
+        return 4;
+      }
+
       i -= character_count;
-      memcpy(strs[word_i], buffer + i, character_count);
+      memcpy(strs[word_i], buffer + i, sizeof(char) * character_count);
+      strs[word_i][character_count] = '\0'; // Null-terminate the string
+      word_i++;
       i += character_count;
-      character_count = -1;
+      character_count = 0;
     }
-    else
+    else if (buffer[i] != '\n')
     {
       character_count++;
     }
   }
 
-  printf("Initial list of words:\n");
+  printf("\nInitial list of words:\n");
   for (i = 0; i < word_count; i++)
   {
     printf("%s ", strs[i]);
   }
 
+  // LSD Radix Sort
+
+  printf("\n\nSorted list of words:\n");
+  for (i = 0; i < word_count; i++)
+  {
+    printf("%s ", strs[i]);
+  }
+
+  // Free all dynamically allocated memory
   free(buffer);
   for (i = 0; i < word_count; i++)
   {
