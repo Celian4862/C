10df3ab9fab103fee6e7e366dedf9dd009e66808
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2025-01-22 14:50:53 +0800

    Modified lsd-radix-sort-strings.c

--- lsd-radix-sort-strings.c
+++ lsd-radix-sort-strings.c
@@ -6,7 +6,7 @@ int main(int argc, char **argv)
 {
   if (argc == 1) // Failsafe case if there aren't enough arguments
   {
-    printf("Not enough arguments; please enter an array of strings separated by a space.\n");
+    printf("Not enough arguments; please enter a file path.\n");
     return 1;
   }
   if (argc > 2) // Failsafe case if there are too many arguments
@@ -15,12 +15,14 @@ int main(int argc, char **argv)
     return 2;
   }
 
-  long file_size,          // Variable to store the file size
-      i;                   // Loop variable
-  int character_count = 0; // Variable to store the number of characters in a word
+  long file_size,           // Variable to store the file size
+      i;                    // Loop variable
+  int character_count = -1, // Variable to store the number of characters in a word
+      word_count = 0,       // Store the number of words to allocate space
+      word_i = 0;           // Iterator for the array of words
   FILE *fp;
 
-  fopen(argv[1], "rb");
+  fopen(argv[1], "rb"); // Any subsequent file operation except fclose leads to segmentation fault... Why?
   if (fp == NULL)
   {
     printf("Error opening file.\n");
@@ -50,16 +52,27 @@ int main(int argc, char **argv)
 
   for (i = 0; i < file_size; i++)
   {
-    if (buffer[i] == ' ')
+    if (buffer[i] == '\n')
+    {
+      word_count++;
+    }
+    else if (buffer[i] == ' ')
     {
       printf("Please format the file to contain words only separated by newlines.\n");
     }
+  }
+
+  char **strs = (char **)malloc(sizeof(char *) * word_count);
+
+  for (i = 0; i < file_size; i++)
+  {
     if (buffer[i] == '\n')
     {
+      strs[word_i++] = (char *)malloc(sizeof(char) * character_count + 1);
       i -= character_count;
-      memcpy(buffer, buffer + i, character_count);
+      memcpy(strs[word_i], buffer + i, character_count);
       i += character_count;
-      character_count = 0;
+      character_count = -1;
     }
     else
     {
@@ -67,7 +80,18 @@ int main(int argc, char **argv)
     }
   }
 
+  printf("Initial list of words:\n");
+  for (i = 0; i < word_count; i++)
+  {
+    printf("%s ", strs[i]);
+  }
+
   free(buffer);
+  for (i = 0; i < word_count; i++)
+  {
+    free(strs[i]);
+  }
+  free(strs);
 
   return 0;
 }
