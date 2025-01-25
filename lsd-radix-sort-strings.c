5ffba9e8017c711877122d0e9d69ff115a9e8676
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2025-01-25 22:05:01 +0800

    Added benchmarking in lsd-radix-sort-strings.c

--- lsd-radix-sort-strings.c
+++ lsd-radix-sort-strings.c
@@ -11,6 +11,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <time.h>
+
 #define ASCII_SIZE 128
 #define ASCII_NULL 0
 #define ASCII_ZERO 48
@@ -35,6 +37,10 @@ int bucket_allocation(char ***, int, int, int, int);
  */
 int main(int argc, char **argv)
 {
+  clock_t start, end;
+  double cpu_time_used;
+  start = clock();
+
   if (argc == 1) // Failsafe case if there aren't enough arguments
   {
     printf("Not enough arguments; please enter a file path.\n");
@@ -191,6 +197,11 @@ int main(int argc, char **argv)
     free(strs[i]);
   }
   free(strs);
+
+  end = clock();
+  cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+  printf("\nTime taken: %lf seconds\n", cpu_time_used);
 
   return 0;
 }
