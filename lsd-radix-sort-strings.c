044f96827f93ec9766ea4b5f65884f5210263b6a
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2025-02-17 15:48:03 +0800

    Updated lsd-radix-sort-strings.c

--- lsd-radix-sort-strings.c
+++ lsd-radix-sort-strings.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 
+#define MAX 128
+
 void counting_sort(char **, const int, const int, const int);
 
 int main(int argc, char **argv)
 {
+  if (argc == 1)
+  {
+    printf("Usage: %s <strings...>\n", argv[0]);
+    return 1;
+  }
   return 0;
 }
 
 void counting_sort(char **arr, const int words, const int word, const int divisor)
 {
-  int countArr[10] = {0}, i;
+  int countArr[MAX] = {0}, i;
   char *outputArr[words];
 
   for (i = 0; i < words; i++)
@@ -17,7 +24,7 @@ void counting_sort(char **arr, const int words, const int word, const int diviso
     countArr[(arr[i][word] / divisor) % 10]++;
   }
 
-  for (i = 0; i < 10; i++)
+  for (i = 0; i < MAX; i++)
   {
     countArr[i] += countArr[i - 1];
   }
